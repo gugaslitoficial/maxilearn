bc1432da64fd584f42b48fdd08ac0c4d6df87227
gugaslitoficial <gustavo-romao@hotmail.com>
2026-06-25 20:58:53 -0300

    feat: cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,13 @@ async function bootstrap() {
   require('dotenv').config();
   const app = await NestFactory.create(AppModule, { bodyParser: true });
   app.use(require('express').json({ limit: '2mb' }));
-  app.enableCors({ origin: 'http://localhost:3000' });
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      process.env.FRONTEND_URL || 'http://localhost:3000',
+    ],
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3001);
